a57396b8f379767e5de3966d030dfe319bd7961b
Bo Frank <bo@bofrank.com>
2020-06-09 13:28:52 -0700

    Changing Schedule to Program.

--- js/components/nav-mobile.js
+++ js/components/nav-mobile.js
@@ -4,7 +4,7 @@ document.getElementById('nav-mobile').innerHTML=`
     <li class="menu_item_mobile"><a href="index.html">Home</a></li>
     <li class="menu_item_mobile"><a href="CFP.html">Call For Papers</a></li>
     <li class="menu_item_mobile"><a href="organizers.html">Organizers</a></li>
-    <li class="menu_item_mobile"><a href="program.html">Schedule</a></li>
+    <li class="menu_item_mobile"><a href="program.html">Program</a></li>
     <li class="menu_item_mobile">
         <div class="dropdown dropdown-mobile">
             <div class="dropbtn">2020 
