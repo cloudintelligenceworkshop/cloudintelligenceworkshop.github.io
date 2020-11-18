01137ba7f04ddd52a02901ee3eec51be6e03bd4c
Bo Frank <bo@bofrank.com>
2020-11-18 10:19:20 -0800

    Archiving 2020 version.
    So users can view the 2020 version of the site.

---
+++ 2020/js/components/nav-mobile.js
@@ -0,0 +1,24 @@
+document.getElementById('nav-mobile').innerHTML=`
+
+<ul>
+    <li class="menu_item_mobile"><a href="index.html">Home</a></li>
+    <li class="menu_item_mobile"><a href="CFP.html">Call For Papers</a></li>
+    <li class="menu_item_mobile"><a href="organizers.html">Organizers</a></li>
+    <li class="menu_item_mobile"><a href="program.html">Program</a></li>
+    <li class="menu_item_mobile">
+        <div class="dropdown dropdown-mobile">
+            <div class="dropbtn">2020 
+                <i class="fa fa-angle-down"></i>
+            </div>
+            <!--
+            <div class="dropdown-content">
+                <a href="2019/index.html">2019</a>
+                <a href="2018/index.html">2018</a>
+                <a href="2017/index.html">2017</a>
+            </div>
+            -->
+        </div> 
+    </li>
+</ul>
+
+`;
