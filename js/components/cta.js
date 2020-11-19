a9dd326533ccf249f58913e3ecda1c60647a7761
Bo Frank <bo@bofrank.com>
2020-11-19 08:45:29 -0800

    Linking contact button to page with form.

--- js/components/cta.js
+++ js/components/cta.js
@@ -6,7 +6,7 @@ document.getElementById('cta').innerHTML=`
 		<div class="col">
 			<div class="cta_title">Need to find us?</div>
 			<div class="button cta_button">
-				<a href="mailto:cloudintelligenceworkshop@gmail.com">Contact</a>
+				<a href="contact.html">Contact</a>
 			</div>
 		</div>
 		</div>
