e7e35de3a96ec9c47413bae5a8aa0b8baec5a46c
Bo Frank <bo@bofrank.com>
2021-12-07 10:14:46 +0900

    Copying files to archive.

---
+++ 2021/js/components/nav.js
@@ -0,0 +1,51 @@
+document.getElementById('nav').innerHTML=`
+
+
+<div class="header_nav" id="header_nav_pin">
+    <div class="header_nav_inner">
+        <div class="header_nav_container">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="header_nav_content d-flex flex-row align-items-center justify-content-start">
+                            <nav class="main_nav">
+                                <ul>
+                                    <li id="home"><a href="index.html">Home</a></li>
+                                    <li id="cfp"><a href="CFP.html">Call For Papers</a></li>
+                                    <li id="papers"><a href="accepted-papers.html">Accepted Papers</a></li>
+                                    <li id="organizers"><a href="organizers.html">Organizers</a></li>
+                                    <li id="program"><a href="program.html">Program</a></li>
+                                    <li>
+                                        <div class="dropdown">
+                                            <div class="dropbtn">2021 
+                                                <i class="fa fa-angle-down"></i>
+                                            </div>
+                                            <div class="dropdown-content">
+                                                <a href="2020/index.html">2020</a>
+                                                <!--
+                                                <a href="2019/index.html">2019</a>
+                                                <a href="2018/index.html">2018</a>
+                                                <a href="2017/index.html">2017</a>
+                                                -->
+                                            </div>
+                                        </div> 
+                                    </li>
+                                </ul>
+                                
+                            </nav>
+
+                            <div class="header_extra ml-auto">
+                                <div class="button header_button">
+                                    <a
+                                        href="https://conf.researchr.org/attending/icse-2021/registration">Register</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+`;
