9aae9cc7575f0eb8c5c91a6c9bc2f0fdfd32e0be
Bo Frank <bo@bofrank.com>
2021-12-17 13:47:19 +0900

    Adding aria labels for accessibilty.

--- js/components/nav-mobile.js
+++ js/components/nav-mobile.js
@@ -1,7 +1,7 @@
 document.getElementById('nav-mobile').innerHTML=`
 
 <ul>
-    <li class="menu_item_mobile"><a href="index.html">Home</a></li>
+    <li class="menu_item_mobile"><a href="index.html" aria-label="home">Home</a></li>
     <li class="menu_item_mobile"><a href="CFP.html">Call For Papers</a></li>
     <!-- <li class="menu_item_mobile"><a href="accepted-papers.html">Accepted Papers</a></li> -->
     <li class="menu_item_mobile"><a href="organizers.html">Organizers</a></li>
@@ -12,12 +12,12 @@ document.getElementById('nav-mobile').innerHTML=`
                 <i class="fa fa-angle-down"></i>
             </div>
             <div class="dropdown-content">
-                <a href="2020/index.html">2021</a>
-                <a href="2020/index.html">2020</a>
+                <a href="2020/index.html" aria-label="2021">2021</a>
+                <a href="2020/index.html" aria-label="2020">2020</a>
                 <!--
-                <a href="2019/index.html">2019</a>
-                <a href="2018/index.html">2018</a>
-                <a href="2017/index.html">2017</a>
+                <a href="2019/index.html" aria-label="2019">2019</a>
+                <a href="2018/index.html" aria-label="2018">2018</a>
+                <a href="2017/index.html" aria-label="2017">2017</a>
                 -->
             </div>
         </div> 
