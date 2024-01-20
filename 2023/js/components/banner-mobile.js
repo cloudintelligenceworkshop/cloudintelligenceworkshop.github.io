b044023a6ee2c3cb009d349840b043160101bbf6
Bo Frank <bo@bofrank.com>
2024-01-20 03:18:18 -0800

    Archiving a copy for 2023.

---
+++ 2023/js/components/banner-mobile.js
@@ -0,0 +1,25 @@
+document.getElementById('banner-mobile').innerHTML=`
+
+<div class="logo menu_logo">
+    <a href="index.html">
+        <div class="logo_container d-flex flex-row align-items-start justify-content-start">
+            <div class="logo_image">
+                <div><img src="images/logo.png" alt="" /></div>
+            </div>
+            <div class="logo_content">
+                <div class="logo_text logo_text_not_ie">
+                    The ICSE'23 Workshop on Cloud Intelligence / AIOps
+                    </div>
+                    <div class="logo_link_mobile">
+                    <a href="https://www.asplos-conference.org/asplos2024/" target="_new">In conjunction with the 45th International Conference on Software Engineering</a>
+                    </div>
+                    <div class="logo_sub">
+                    May 15th, 2023 Melbourne Convention and Exhibition Center
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
+
+`;
---
+++ 2023/js/components/nav.js
@@ -0,0 +1,56 @@
+document.getElementById('nav').innerHTML=`
+
+
+<div class="header_nav" id="header_nav_pin">
+    <div class="header_nav_inner">
+        <div class="header_nav_container">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="header_nav_content d-flex flex-row align-items-center justify-content-start">
+                            <nav class="main_nav">
+                                <ul>
+                                    <li id="home"><a href="index.html" aria-label="home">Home</a></li>
+                                    <li id="cfp"><a href="CFP.html">Call for Papers</a></li>
+                                    <!--
+                                    <li id="papers"><a href="accepted-papers.html">Accepted Papers</a></li>
+                                    -->
+                                    <li id="organizers"><a href="organizers.html">Organizers</a></li>
+                                    <li id="program"><a href="program.html">Program</a></li>
+                                    <li>
+                                        <div class="dropdown">
+                                            <div class="dropbtn">2023 
+                                                <i class="fa fa-angle-down"></i>
+                                            </div>
+                                            <div class="dropdown-content">
+                                                <a href="2022/index.html" aria-label="2022">2022</a>
+                                                <a href="2021/index.html" aria-label="2021">2021</a>
+                                                <a href="2020/index.html" aria-label="2020">2020</a>
+                                                <!--
+                                                <a href="2019/index.html" aria-label="2019">2019</a>
+                                                <a href="2018/index.html" aria-label="2018">2018</a>
+                                                <a href="2017/index.html" aria-label="2017">2017</a>
+                                                -->
+                                            </div>
+                                        </div> 
+                                    </li>
+                                </ul>
+                                
+                            </nav>
+                            <div class="header_extra ml-auto">
+                            <!--
+                                <div class="button header_button">
+                                    <a
+                                        href="https://icmsaust.eventsair.com/icse-2023/registration-form/Site/Register">Register</a>
+                                </div>
+                            -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+`;
