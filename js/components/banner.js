314d4676be534037196fae274d0a9ad5d8cf44cc
Bo Frank <bo@bofrank.com>
2024-01-27 16:32:19 -0800

    Adjusting text and style for header, contact email

--- js/components/banner.js
+++ js/components/banner.js
@@ -15,10 +15,10 @@ document.getElementById('banner').innerHTML=`
                                 </div>
                                 <div class="logo_content">
                                     <div id="logo_text" class="logo_text logo_text_not_ie">
-                                        The ASPLOS'24 Workshop on Cloud Intelligence / AIOps
+                                    5th International Workshop on Cloud Intelligence / AIOps (AIOps '24)
                                         </div>
                                         <div class="logo_link">
-                                        <a href="https://www.asplos-conference.org/asplos2024/" target="_new">In conjunction with the ACM International Conference on Architectural Support for Programming Languages and Operating Systems</a>
+                                        <a href="https://www.asplos-conference.org/asplos2024/" target="_new">Co-located with ASPLOS '24 </a>
                                         </div>
                                         <div class="logo_sub">
                                         April 27th, 2024 San Diego
