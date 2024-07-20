ef7714929bbbf453d1cd416694a4c8089ae1654f
Bo Frank <bo@bofrank.com>
2024-07-19 19:39:39 -0700

    Copying files.

---
+++ 2024/js/components/banner-mobile.js
@@ -0,0 +1,24 @@
+document.getElementById('banner-mobile').innerHTML=`
+
+<div class="logo menu_logo">
+    <a href="index.html">
+        <div class="logo_container d-flex flex-row align-items-start justify-content-start">
+            <div class="logo_image">
+                <div><img src="images/logo.png" alt="" /></div>
+            </div>
+            <div class="logo_content">
+                <div class="logo_text logo_text_not_ie">
+                5th International Workshop on Cloud Intelligence / AIOps (AIOps '24) 
+                    </div>
+                    <div class="logo_link_mobile">
+                    <a href="https://www.asplos-conference.org/asplos2024/" target="_new">Co-located with ASPLOS '24</a>
+                    </div>
+                    <div class="logo_sub">
+                    April 27th, 2024 San Diego
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
+
+`;
